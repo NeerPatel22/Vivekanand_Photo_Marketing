cb00a7a3e424a10461a6b5c91899fd40a0b3490c
Neer Patel <patelneer8@gmail.com>
2026-01-12 23:16:39 -0500

    model update with compress image

--- vpm/models.py
+++ vpm/models.py
@@ -4,6 +4,9 @@ from django.db import models
 import os
 from uuid import uuid4
 from django.utils.text import slugify
+from PIL import Image
+from io import BytesIO
+from django.core.files import File
 import datetime
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
@@ -11,7 +14,7 @@ from django.dispatch import receiver
 def rename_image(instance, filename):
     ext = filename.split('.')[-1]
     if instance.ProductImage:
-        name= slugify(instance.ProductName)
+        name = slugify(instance.ProductName)
     else:
         name = uuid4().hex[:10]
     image_name = f'{name}.{ext}'
@@ -36,3 +39,24 @@ class Product(models.Model):
     ProductDescription = models.TextField()
     ProductPrice = models.CharField(max_length=50)
     ProductImage = models.ImageField(upload_to=rename_image, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.ProductImage:
+            img = Image.open(self.ProductImage)
+
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+
+            if img.height > 800 or img.width > 800:
+                output_size = (800, 800)
+                img.thumbnail(output_size)
+
+            output = BytesIO()
+            img.save(output, format='JPEG', quality=70)
+            output.seek(0)
+
+            name = rename_image(self, self.ProductImage.name)
+
+            self.ProductImage.save(name, File(output), save=False)
+
+        super().save(*args, **kwargs)
--- vpm/views.py
+++ vpm/views.py
@@ -1,5 +1,4 @@
 from os import P_NOWAIT
-
 from django.shortcuts import render
 import requests
 from django.shortcuts import render, redirect

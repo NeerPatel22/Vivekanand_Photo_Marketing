fb684b508ff9fbdd3d7c9db51958a238b4759e5b
Neer Patel <patelneer8@gmail.com>
2026-02-01 22:15:57 -0500

    deploy

--- Vivekanand_Photo_Marketing/settings.py
+++ Vivekanand_Photo_Marketing/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['80.225.255.15', 'localhost', '127.0.0.1']
 
 
 # Application definition
